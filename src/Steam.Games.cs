2689e2886f756b85415133724e10febf6d2dd9f0
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2025-07-03 14:49:19 -0500

    Update Steam.Games.cs

--- src/Steam.Games.cs
+++ src/Steam.Games.cs
@@ -250,6 +250,8 @@ public partial class Steam
 
 			if (appRequests.Count > 0)
 			{
+				Console.WriteLine("Getting app info for " + appRequests.Count + " games");
+				
 				var appInfo = await steamApps.PICSGetProductInfo(appRequests, []);
 
 				foreach (var appResult in appInfo.Results)
